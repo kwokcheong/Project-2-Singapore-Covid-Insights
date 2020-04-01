29075499434b5331fa995c89eb8324c5c746caa9
Kwok cheong <wongkwokcheong@hotmail.com>
2020-04-01 16:38:43 +0800

    Updated table for CSV data, added graph templates

--- script.js
+++ script.js
@@ -31,12 +31,19 @@ axios.get('sghospitals.json').then(function(hosptitals){
 //Lets load in the CSV FILES
 $(function(){
 
+
+  
 axios.get('covidcase.csv').then(function(response){
     csv().fromString(response.data).then(function(data){
       console.table(data);
       for (let i in data){
         $("#cases").append(`
-            <li> ${data[i].Hospital} </li>
+            <tr> 
+             <td>${data[i].Cases} </td>
+             <td>${data[i].Age} </td>
+             <td>${data[i].Gender}</td>
+             <td>${data[i].Hospital} </td>
+            </tr>
         `)
       }
     });
@@ -44,3 +51,63 @@ axios.get('covidcase.csv').then(function(response){
 
 
 })
+
+
+//My Chart Graphs
+
+let myChart = document.getElementById('myChart').getContext('2d');
+
+let PopChart = new Chart(myChart,{
+  type:'horizontalBar', //can be bar, horizontalbar, pie, line, doughnut,radar, polar area
+  data:{
+    labels:['NUH','CGH','NTFGH','SGH','NCID','SKH','KTPH','AH','FPH','GEH','KKH','KTPH','MEH','MENH'],
+    datasets:[{
+      label: 'Population',
+      data:[
+        30,
+        20,
+        40,
+        40,
+        50,
+        30,
+        23,
+        41,
+        34,
+        32,
+        13,
+        44
+      ],
+      backgroundColor: 'lightgreen'
+    }]
+  },
+  options:{}
+});
+
+
+let myGraph = document.getElementById('myGraph').getContext('2d');
+
+let dataChart = new Chart(myGraph,{
+  type:'line', //can be bar, horizontalbar, pie, line, doughnut,radar, polar area
+  data:{
+    labels:['NUH','CGH','NTFGH','SGH','NCID','SKH','KTPH','AH','FPH','GEH','KKH','KTPH','MEH','MENH'],
+    datasets:[{
+      label: 'Population',
+      data:[
+        30,
+        20,
+        40,
+        40,
+        50,
+        30,
+        23,
+        41,
+        34,
+        32,
+        13,
+        44
+      ],
+      backgroundColor: 'violet'
+    }]
+  },
+  options:{}
+});
