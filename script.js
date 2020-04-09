1bb3d4181960eb8e78ca60e105dea00fca1103da
Kwok cheong <wongkwokcheong@hotmail.com>
2020-04-09 16:50:43 +0800

    Added pulsating markers to clusters, added new cluster zones and coordinates

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
-//global variable Hospital Information
+//Hospital data 
 let hospitalarray = [];
 let hospitalCount = [0];
 let hospitalDict = [{
   name: "n/a",
   count: 0
 }];
+//Gender data 
 let genderCount = [0];
 let male = 0;
 let female = 0;
+
+//Age data
+//Indexes : 0-14, 15-24, 25-54, 55-64, 65>
+let ageArray = [0,0,0,0,0];
+
 
 // 1. Map Leaflet
 let map = createMap("map", [1.3521, 103.8198], 13);
@@ -24,16 +30,34 @@ axios.get('sghospitals.json').then(function (hospitals) {
   map.layers(hospitalGroup).addTo(map);
 });
 
-//this is an example of putting NUH in
-// let NUHCoord = [1.2966,103.7764];
-// let NUHCircle = L.circle(NUHCoord,{
-//     'color':'blue',
-//     'fillColor':'blue',
-//     'fillOpacity':0.3,
-//     'radius':800
-// }).addTo(map);
 
-// NUHCircle.bindPopup(`<p>National University Hopstital</p>`)
+PulsatingMarker = function (radius, color) {
+    const circlestyle = `
+      width: ${radius}px;
+      height: ${radius}px;
+      fillOpacity: 0.3,
+      background: ${color};
+      color: ${color};
+    `
+    return L.divIcon({
+      html: `<span style="${circlestyle}" class="pulse"/>`,
+      className: ''
+    })
+  }
+
+pulsatingIcon = PulsatingMarker(50, 'rgba(247, 0, 0, 0.533)');
+
+axios.get('sgclusters.json').then(function (clusters) {
+    let clusterGroup = L.layerGroup();
+    for (let cluster of clusters.data) {
+      let circle = L.marker(cluster.coordinates, {icon: pulsatingIcon} 
+        ).bindPopup(`<h7>${cluster.name}</h7>`);
+      circle.addTo(map);
+    }
+    map.layers(clusterGroup).addTo(map);
+  });
+
+  
 
 
 // 2. Update hospital array and dictionary counter and print table out
@@ -42,6 +66,8 @@ axios.get('covidcase.csv').then(function (response) {
   csv().fromString(response.data).then(function (data) {
 
     //console.table(data);
+
+    //hospital case count
     for (let i in data) {
       hospitalDict[data[i].Hospital] = hospitalDict[data[i].Hospital] + 1;
 
@@ -51,6 +77,20 @@ axios.get('covidcase.csv').then(function (response) {
       }else if (data[i].Gender === "F"){
           female = female + 1;
       }
+
+      //age count
+      if (data[i].Age >= 0 && data[i].Age <= 14){
+          ageArray[0] +=  1;
+      } else if (data[i].Age >= 15 && data[i].Age <=24 ){
+          ageArray[1] += 1;
+      } else if (data[i].Age >= 25 && data[i].Age <=54 ){
+          ageArray[2] += 1;
+      } else if (data[i].Age >=55 && data[i].Age <=64){
+          ageArray[3] += 1;
+      }else{
+          ageArray[4] += 1;
+      }
+
       
       $("#cases").append(`
             <tr> 
@@ -75,7 +115,6 @@ axios.get('covidcase.csv').then(function (response) {
   });
     
 });
-
 
 
 // 3. Chart.js
@@ -172,19 +211,19 @@ console.log(genderCount[0])
 
 var ctx3 = document.getElementById('myAge');
 var myChart3 = new Chart(ctx3, {
-    type: 'bar',
+    type: 'polarArea',
     data: {
         labels: ['Male', 'Female'],
         datasets: [{
-            label: 'age',
+            label: 'Gender',
             data: [genderCount[0], genderCount[1]],
             backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
                 'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 99, 132, 0.2)',
             ],
             borderColor: [
-                'rgba(255, 99, 132, 1)',
                 'rgba(54, 162, 235, 1)',
+                'rgba(255, 99, 132, 1)',
             ],
             borderWidth: 1
         }]
@@ -192,39 +231,37 @@ var myChart3 = new Chart(ctx3, {
     options: {
       reponsive: true,
       maintainAspectRatio: false,
-        scales: {
-            yAxes: [{
-                ticks: {
-                    beginAtZero: true
-                }
-            }]
-        }
+        // scales: {
+        //     yAxes: [{
+        //         ticks: {
+        //             beginAtZero: true
+        //         }
+        //     }]
+        // }
     }
 });
 
 var ctx4 = document.getElementById('myGender');
 var myChart4 = new Chart(ctx4, {
-    type: 'pie',
+    type: 'bar',
     data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        labels: ['0-14', '15-24', '25-54', '54-65', '>65'],
         datasets: [{
-            label: 'Gender Demographic of Cases',
-            data: [12, 19, 3, 5, 2, 3],
+            label: 'Age group of Cases',
+            data: ageArray,
             backgroundColor: [
                 'rgba(255, 99, 132, 0.2)',
                 'rgba(54, 162, 235, 0.2)',
                 'rgba(255, 206, 86, 0.2)',
                 'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
+                'rgba(153, 102, 255, 0.2)'
             ],
             borderColor: [
                 'rgba(255, 99, 132, 1)',
                 'rgba(54, 162, 235, 1)',
                 'rgba(255, 206, 86, 1)',
                 'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
+                'rgba(153, 102, 255, 1)'
             ],
             borderWidth: 1
         }]
